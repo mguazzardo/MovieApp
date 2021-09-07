a2c4e32aaad553e8d340d562de041dc25df9db61
BereR <berenice_reddig@hotmail.com>
2021-09-06 21:56:56 -0300

    Routing with React Router + BreadCrumb Component + MovieInfo Component

---
+++ react-rmdb/src/components/Movie.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+//config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+
+//components
+import BreadCrumb from './BreadCrumb';
+import Grid from './Grid';
+import Spinner from './Spinner';
+import MovieInfo from './MovieInfo';
+
+//Hook
+import { useMovieFetch } from '../hooks/useMovieFetch';
+
+//Image
+import NoImage from '../images/no_image.jpg';
+
+const Movie = () => {
+    const { movieId } = useParams();
+    const { state: movie, loading, error } = useMovieFetch(movieId);
+    
+    if(loading) return <Spinner />;
+    if(error) return <div>Somenthing went wrong...</div>;
+
+    return (
+        <>
+            <BreadCrumb movieTitle={movie.original_title} />
+            <MovieInfo movie={movie} />
+        </>
+    );
+};
+
+export default Movie;
